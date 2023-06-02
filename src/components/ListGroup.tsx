5f0ee8f155e2bcd7435481809a17add622ed3839
DEV yasmin <y282ahmed@gmail.com>
2023-06-02 16:39:09 +0300

    commit react project v1

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -15,7 +15,7 @@ function ListGroup() {
   return (
     <>
       <h1>List Group</h1>
-      {items.length === 0 ? <p>No items found</p> : null}
+      {/* {items.length === 0 ? <p>No items found</p> : null} */}
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item) => (
