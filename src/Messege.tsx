288aa782214285d2d84c70d7fd6d81809b40fefc
DEV yasmin <y282ahmed@gmail.com>
2023-06-02 16:27:57 +0300

    commit is react app v1

---
+++ src/Messege.tsx
@@ -0,0 +1,7 @@
+function Messege() {
+  const name = "Ahmed I love U and don't be upset from me Iam so sorry my Dad";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello world</h1>;
+}
+
+export default Messege;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,31 @@
+function ListGroup() {
+  let items = ["paris", "New york", "ahmed", "yasmin"];
+  items = [];
+
+  //if(items.length===0)
+  //return(
+  //  <>
+  //  <h1>List Group</h1>
+  //  <p>Not found</p>
+  //  </>
+  //)
+
+  //const getMessage=() => {
+  //  return items.length===0 ? <p>No items found</p> : null;;}
+  return (
+    <>
+      <h1>List Group</h1>
+      {items.length === 0 ? <p>No items found</p> : null}
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
